251a608e20aaec16d7fbfcf91954246c2f96cef4
philipboboc1-debug <philipboboc1@gmail.com>
2026-03-28 00:48:38 -0500

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,12 +1,12 @@
+/* Ultraviolet Configuration for Vercel 2026 */
 self.__uv$config = {
-    prefix: "/uv/service/",
-    /* This server is currently the most stable for Vercel/Home PC */
-    bare: "https://uv.student-info.xyz/bare/", 
+    prefix: '/uv/service/',
+    bare: 'https://uv.student-info.xyz/bare/', 
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: "/uv/uv.handler.js",
-    client: "/uv/uv.client.js",
-    bundle: "/uv/uv.bundle.js",
-    config: "/uv/uv.config.js",
-    sw: "/uv/uv.sw.js",
+    handler: '/uv/uv.handler.js',
+    client: '/uv/uv.client.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
 };
